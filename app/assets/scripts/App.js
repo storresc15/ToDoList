00722f1a6383f5861bcb13b2e5ff15a389344472
Sebas <sebas.torres.88@gmail.com>
2019-05-02 22:32:24 -0500

    The second commit, includes the intial HTML and packages

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,4 @@
-import MobileMenu from './modules/MobileMenu';
+/*import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
 import StickyHeader from './modules/StickyHeader';
@@ -9,3 +9,15 @@ new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
 var stickyHeader = new StickyHeader();
 var modal = new Modal();
+*/
+import $ from 'jquery';
+$("li").click(function() {
+$(this).toggleClass("completed");
+});
+
+$("span").click(function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
